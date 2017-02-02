d3e59709fe98103261c67edd62bcba4b1b223a5d
Peter Nguyen <pnguyen339@gmail.com>
2017-02-01 18:59:27 -0600

    starting on the code

---
+++ Lab1/assembler.c
@@ -0,0 +1,89 @@
+#include <stdio.h> /* standard input/output library */
+#include <stdlib.h> /* Standard C Library */
+#include <string.h> /* String operations library */
+#include <ctype.h> /* Library for useful character operations */
+#include <limits.h> /* Library for definitions of common variable type characteristics */
+#define MAX_LINE_LENGTH 255
+#define ADD "add"
+#define AND "and"
+#define BR "br"
+#define BRNZP "brnzp"
+#define BRN "brn"
+#define BRP "brp"
+#define BRZ "brz"
+#define BRZP "brzp"
+#define BRNP "brnp"
+#define BRNZ "brnz"
+#define HALT "halt"
+#define JMP "jmp"
+#define JSR "jsr"
+#define JSRR "jsrr"
+#define LDB "ldb"
+#define LDW "ldw"
+#define LEA "lea"
+#define NOP "nop"
+
+int isOpcode(char *ptr)
+	{	
+		
+
+
+
+	}
+
+enum
+	{
+	   DONE, OK, EMPTY_LINE
+	};
+
+
+int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char
+	** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4
+	)
+	{
+	   char * lRet, * lPtr;
+	   int i;
+	   if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
+		return( DONE );
+	   for( i = 0; i < strlen( pLine ); i++ )
+		pLine[i] = tolower( pLine[i] );
+	   
+           /* convert entire line to lowercase */
+	   *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+
+	   /* ignore the comments */
+	   lPtr = pLine;
+
+	   while( *lPtr != ';' && *lPtr != '\0' &&
+	   *lPtr != '\n' ) 
+		lPtr++;
+
+	   *lPtr = '\0';
+	   if( !(lPtr = strtok( pLine, "\t\n ," ) ) ) 
+		return( EMPTY_LINE );
+
+	   if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
+	   {
+		*pLabel = lPtr;
+		if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	   }
+	   
+           *pOpcode = lPtr;
+
+	   if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	   
+           *pArg1 = lPtr;
+	   
+           if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	   *pArg2 = lPtr;
+	   if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	   *pArg3 = lPtr;
+
+	   if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+	   *pArg4 = lPtr;
+
+	   return( OK );
+	}
