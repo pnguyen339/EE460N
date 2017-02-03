6a2b2e42fc62f45f8281427ec1a6ffcb077300dc
Peter Nguyen <pnguyen339@gmail.com>
2017-02-03 12:37:02 -0600

    finish SymbolTable

--- Lab1/SymbolTable.c
+++ Lab1/SymbolTable.c
@@ -1,12 +1,42 @@
+#include <limits.h> /* Library for definitions of common variable type characteristics */
+#include <string.h> /* String operations library */
+#include <stdlib.h>
 
-
-
-
-struct SymbolTable{
-	char name[256];
+typedef struct SymbolTable{
+	char* name;
 	int location;
-	SymbolTable *nextptr;
+	struct SymbolTable *nextptr;
 
-};
+}symbol ;
 
-SymbolTable* newSymbol
+void newSymbol(symbol* strt, int* length, char* Label, int loc){
+	int i;
+	for(i=0; i<*length; i++){
+		strt=strt->nextptr;
+	}
+	
+	strt = (symbol*) malloc(sizeof(char)*strlen(Label)+1+sizeof(int)+sizeof(symbol*));
+	strt->name = Label;
+	strt->location = loc;
+	strt->nextptr = NULL;
+	*length++;
+
+}
+
+
+int findSym(symbol* strt, int length,char* Label){
+	int i;
+	for(i =0; i < length; i++){
+		if(strcmp(strt->name, Label) == 0)
+			return strt->location;
+		else
+			strt = strt->nextptr;
+	}
+	return -1;
+}
+
+void destroy(symbol* strt){
+	if(strt->nextptr != NULL)
+		destroy(strt->nextptr);
+	free(strt);
+}
