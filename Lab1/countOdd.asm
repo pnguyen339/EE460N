a773e889e45e7a787f11bdcba0974cd25b04c1c1
Eric Wang <EricBBWang@GMail.com>
2017-02-05 21:42:01 -0600

    updated countOdd.asm

--- Lab1/countOdd.asm
+++ Lab1/countOdd.asm
@@ -2,13 +2,13 @@
 	AND R0, R0, #0		; result in R0
 
 	AND R1, R1, #0
-	ADD R1, R1, #256	; loop counter in R1
+	ADD R1, R1, #1		; loop counter in R1
+	LSHF R1, R1, #8
 
 	LEA R2, ADR, #0		; pointer in R2
 	LDW R2, R2, #0
 
-LOOP
-	LDB R3, R2, #0		; number
+LP	LDB R3, R2, #0		; number
 	ADD R2, R2, #1		; increment pointer
 
 	AND R3, R3, #1		; increment if odd
