685f3901a8aca97e8d3148726b11b90e6ea98dfd
Eric Wang <EricBBWang@GMail.com>
2017-02-05 23:35:32 -0600

    Combined into single file

--- Lab1/assembler.c
+++ Lab1/assembler.c
@@ -3,12 +3,85 @@
 #include <string.h> /* String operations library */
 #include <ctype.h> /* Library for useful character operations */
 #include <limits.h> /* Library for definitions of common variable type characteristics */
-#include "SymbolTable.h"
+
 #define MAX_LINE_LENGTH 255
 #define InstBitLen 16
 #define ASCIItoNum 48
 FILE* infile = NULL;
 FILE* outfile = NULL;
+
+typedef struct SymbolTable {
+	int location;
+	char* name;
+	struct SymbolTable *nextptr;
+} symbol;
+
+static int length = 0;
+
+void newSymbol(symbol* strt, char* Label, int loc) {
+	int i;
+	int labellen;
+	for (i = 0; i < length; i++) {
+		strt = strt->nextptr;
+	}
+
+	symbol* newsymbol = (symbol*)malloc(sizeof(symbol));
+	strt->nextptr = newsymbol;
+
+	labellen = strlen(Label);
+	newsymbol->name = strncpy((char*)malloc(labellen + 1), Label, labellen);
+	newsymbol->location = loc;
+	newsymbol->nextptr = NULL;
+	length++;
+}
+
+symbol* initTable(void) {
+	symbol* sym_table = (symbol*)malloc(sizeof(symbol));
+	symbol* sym = sym_table;
+
+	sym->location = -1;
+	sym->name = strncpy((char*)malloc(3), "in", 2);			/* IN, OUT, GETC, PUTS are all invalid opcodes */
+	sym = (sym->nextptr = (symbol*)malloc(sizeof(symbol)));
+
+	sym->location = -1;
+	sym->name = strncpy((char*)malloc(3), "out", 2);
+	sym = (sym->nextptr = (symbol*)malloc(sizeof(symbol)));
+
+	sym->location = -1;
+	sym->name = strncpy((char*)malloc(3), "getc", 2);
+	sym = (sym->nextptr = (symbol*)malloc(sizeof(symbol)));
+
+	sym->location = -1;
+	sym->name = strncpy((char*)malloc(3), "puts", 2);
+	sym = NULL;
+
+	return sym_table;
+}
+
+
+int findSym(symbol* strt, char* Label) {
+	int i;
+	for (i = 0; i <= length; i++) {
+		if (strcmp(strt->name, Label) == 0)
+			return strt->location;
+		else
+			strt = strt->nextptr;
+	}
+	return -1;
+}
+
+void destroy(symbol* strt) {
+	symbol* temp;
+	while (strt->nextptr != NULL) {
+		temp = strt->nextptr;
+
+		free(strt->name);
+		free(strt);
+		strt = temp;
+	}
+	free(strt);
+}
+
 
 symbol* sym_table;
 
