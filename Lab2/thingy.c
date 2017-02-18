c8d1d54d5ddd1855b4e073257181f43d3cfeccd5
Eric Wang <EricBBWang@GMail.com>
2017-02-17 23:45:29 -0600

    Made a few things more accurate, rounded out last defines

--- Lab2/thingy.c
+++ Lab2/thingy.c
@@ -13,23 +13,35 @@ int MEMORY[WORDS_IN_MEM][2];
 	//	int REGS[LC_3b_REGS];  /* register file. */
 	//} System_Latches;
 
+/********************* DEFINES ***********************/
+
+/* Useful constants */
 #define NUM_INSTR	16
+
+/* Opcdoe */
 #define DECODE(x)	((x & 0xF000u) >> 12)
 
+/* Operands within instruction */
 #define OP1(x)		((x & 0x0E00u) >> 9)
 #define OP2(x)		((x & 0x01C0u) >> 6)
 #define OP3(x)		((x & 0x0007u) >> 0)
 
+/* Immediate values at end of instruction */
 #define CONST_11(x)	((x & 0x07FFu) >> 0)
 #define CONST_9(x)	((x & 0x01FFu) >> 0)
 #define CONST_5(x)	((x & 0x001Fu) >> 0)
-//#define CONST_4(x)	((x & 0x000Fu) >> 0)	// don't actually need -- can interpret sign bit of CONST_5 for SHF, as amount is unsigned
-//(DO NOT LEFT SHIFT A NEGATIVE AMOUNT)
+#define CONST_4(x)	((x & 0x000Fu) >> 0)
+
+/* Bits used for steering or otherwise important values */
+#define SIGN_BIT(x)	((x & 0x8000u) >> 15)
 
 #define BIT_11(x)	((x & 0x0800u) >> 11)
 #define BIT_10(x)	((x & 0x0400u) >> 10)
 #define BIT_9(x)	((x & 0x0200u) >> 9)
 #define BIT_5(x)	((x & 0x0020u) >> 5)
+
+/******************* END DEFINES *********************/
+
 
 int sext(int input, int num_bits) {
 
@@ -50,13 +62,32 @@ System_Latches op_br(int instr) {
 
 System_Latches op_add(int instr) {
 	System_Latches next = CURRENT_LATCHES;
+	int result;
 
-	if (BIT_5(instr)) {				// immediate value
-		next.REGS[OP1(instr)] = Low16bits(next.REGS[OP2(instr)] + sext(CONST_5(instr), 5));
+	if (BIT_5(instr)) {				/* immediate value */
+		result = next.REGS[OP2(instr)] + sext(CONST_5(instr), 5);
 	}
-	else {							// register
-		next.REGS[OP1(instr)] = Low16bits(next.REGS[OP2(instr)] + next.REGS[OP3(instr)]);
+	else {							/* register */
+		result = next.REGS[OP2(instr)] + next.REGS[OP3(instr)];
 	}
+
+	result = Low16bits(result);
+
+	next.N = 0;						/* condition codes */
+	next.Z = 0;
+	next.P = 0;
+	
+	if (result == 0) {
+		next.Z = 1;
+	}
+	else if (SIGN_BIT(result)) {
+		next.N = 1;
+	}
+	else {
+		next.P = 1;
+	}
+
+	return next;
 }
 
 System_Latches op_ldb(int instr) {
